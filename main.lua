03490963a831d048da10cf96a4edbe623660c82f
ThorinWittaker <dowit@hotmail.com>
2013-11-21 14:17:43 -0500

    This project is failing on my Galaxy Tab II 7 inch, but it runs fine in the Corona Sim 2013.2076

--- main.lua
+++ main.lua
@@ -1,3 +1,16 @@
 
-local myCirle = display.newCircle(display.contentCenterX, display.contentCenterY, 200)
+print('circleTest: before calling the circle')
+
+local myCircle = display.newCircle(display.contentCenterX, display.contentCenterY, 200)
+myCircle:setFillColor(0, 1, 1, 0.5)
+
+print('circleTest: after calling the circle')
+
+
+local myRectangle = display.newRect(10, 10, 50, 50)
+myRectangle:setFillColor(1, 1, 0, 1)
+
+local myImage = display.newImageRect('shape.png', 50, 50)
+myImage.x = display.contentCenterX
+myImage.y = display.contentCenterY
 
